c302c914ac4da176c241f79f6b6dfaaccbdad1f3
Diana Serradilla <dianas@itcsoluciones.com>
2022-07-05 11:19:45 -0300

    prueba GYRO

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("c 5")
+alert("a 5")
 // Get reference to Canvas
 var canvas = document.getElementById('canvas');
 
@@ -296,11 +296,11 @@ window.addEventListener('deviceorientation', function (event) {
     // The device is right-side up in portrait orientation
     motion.x = event.gamma - motion_initial.y;
     motion.y = event.beta - motion_initial.x;
-  } else if (window.orientation === 70) {
+  } else if (window.orientation === 50) {
     // The device is in landscape laying on its left side
     motion.x = event.beta - motion_initial.x;
     motion.y = -event.gamma + motion_initial.y;
-  } else if (window.orientation === -70) {
+  } else if (window.orientation === -50) {
     // The device is in landscape laying on its right side
     motion.x = -event.beta + motion_initial.x;
     motion.y = event.gamma - motion_initial.y;
